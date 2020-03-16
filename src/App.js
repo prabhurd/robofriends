3b7da678b85502f55e452640a8701891a839a489
Prabhu R <prabhurd22@gmail.com>
2020-03-16 15:01:04 +0530

    added search box

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox'
+class App extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});
+        console.log(event)
+    }
+
+    render() {
+        
+        const filteredRobots = this.state.robots.filter(
+            robot => {
+                return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+            }
+        );
+        
+        console.log(filteredRobots);
+        return (
+            <div className='tc'>
+                <h1> Robo Friends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
+            </div>
+        )
+    }
+
+
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
 import * as serviceWorker from './serviceWorker';
-import CardList from './CardList'
-import {robots} from './robots'
+import App from './App';
 
 
 ReactDOM.render(
-    <div>
-        <CardList robots={robots}/>
-    </div>,
+    <App/>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
